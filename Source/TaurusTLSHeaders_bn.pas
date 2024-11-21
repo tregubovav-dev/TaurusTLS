5c568b54e823998e8b11528b0366f0896b9538b0
J. Peter Mugaas <jpmugaas@suddenlink.net>
2024-11-21 12:20:37 -0500

    Fix compiler error.

--- Source/TaurusTLSHeaders_bn.pas
+++ Source/TaurusTLSHeaders_bn.pas
@@ -944,8 +944,7 @@ implementation
 
   uses
     classes, 
-    TaurusTLSExceptionHandlers,
-    TaurusTLS_ResourceStrings
+    TaurusTLSExceptionHandlers
   {$IFNDEF OPENSSL_STATIC_LINK_MODEL}
     ,TaurusTLSLoader
   {$ENDIF};

ec9f50ee60263934ac36ec4bec44cb2157fd90ef
J. Peter Mugaas <jpmugaas@suddenlink.net>
2024-12-09 14:19:22 -0500

    Keep files in sync with indy_extras repository.

---
+++ Tests/Tests.Http.pas
@@ -0,0 +1,40 @@
+﻿unit Tests.Http;
+
+interface
+
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  TestHttp = class
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure BasicConnection;
+  end;
+
+implementation
+
+procedure TestHttp.Setup;
+begin
+//Todo
+end;
+
+procedure TestHttp.TearDown;
+begin
+//Todo
+end;
+
+procedure TestHttp.BasicConnection;
+begin
+//Todo
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TestHttp);
+end.

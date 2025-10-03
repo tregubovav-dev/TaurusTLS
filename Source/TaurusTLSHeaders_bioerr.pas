d72dea853dd2d8c18d975839ce83f52c1a4c8b7e
Alexander Tregubov (aka @AT) <tregubovav.dev@gmail.com>
2025-10-02 22:57:38 -0700

    TaurusTLS_Headers_bioerr: cdecl and typed procedural var assignments
    Issue: procedure Load in the TaurusTLS_Headers_bioerr unit
    assigns untyped pointers to procedural variables which
    may introduce invalid calling conventions.
    Fix: all assignment are typed
    
    Issue: not every internal ERR_xxx, FC_xxx and _xxx function
    has "cdecl" directive. This introduce invalid calling convention.
    Fix: "cdecl" directive added.

--- Source/TaurusTLSHeaders_bioerr.pas
+++ Source/TaurusTLSHeaders_bioerr.pas
@@ -169,7 +169,7 @@ const
 
 
   {$I TaurusTLSNoRetValOff.inc} 
-function  ERR_ERR_load_BIO_strings: TIdC_INT; 
+function  ERR_ERR_load_BIO_strings: TIdC_INT; cdecl;
 begin
   ETaurusTLSAPIFunctionNotPresent.RaiseException(ERR_load_BIO_strings_procname);
 end;
@@ -188,13 +188,13 @@ begin
   if FuncLoadError then
   begin
     {$if not defined(ERR_load_BIO_strings_allownil)}
-    ERR_load_BIO_strings := @ERR_ERR_load_BIO_strings;
+    ERR_load_BIO_strings := ERR_ERR_load_BIO_strings;
     {$ifend}
     {$if declared(ERR_load_BIO_strings_introduced)}
     if LibVersion < ERR_load_BIO_strings_introduced then
     begin
       {$if declared(FC_ERR_load_BIO_strings)}
-      ERR_load_BIO_strings := @FC_ERR_load_BIO_strings;
+      ERR_load_BIO_strings := FC_ERR_load_BIO_strings;
       {$ifend}
       FuncLoadError := false;
     end;
@@ -203,7 +203,7 @@ begin
     if ERR_load_BIO_strings_removed <= LibVersion then
     begin
       {$if declared(_ERR_load_BIO_strings)}
-      ERR_load_BIO_strings := @_ERR_load_BIO_strings;
+      ERR_load_BIO_strings := _ERR_load_BIO_strings;
       {$ifend}
       FuncLoadError := false;
     end;

e2eb2e2b90931bc5861911c7dadf525905e70bba
Alexander Tregubov (aka @AT) <tregubovav.dev@gmail.com>
2025-10-02 23:16:50 -0700

    TaurusTLS_Headers_bnerr: cdecl and typed procedural var assignments
    Issue: procedure Load in the TaurusTLS_Headers_bnerr unit
    assigns untyped pointers to procedural variables which
    may introduce invalid calling conventions.
    Fix: all assignment are typed
    
    Issue: not every internal ERR_xxx, FC_xxx and _xxx function
    has "cdecl" directive. This introduce invalid calling convention.
    Fix: "cdecl" directive added.

--- Source/TaurusTLSHeaders_bnerr.pas
+++ Source/TaurusTLSHeaders_bnerr.pas
@@ -145,7 +145,7 @@ const
 
 
   {$I TaurusTLSNoRetValOff.inc} 
-function  ERR_ERR_load_BN_strings: TIdC_INT; 
+function  ERR_ERR_load_BN_strings: TIdC_INT; cdecl;
 begin
   ETaurusTLSAPIFunctionNotPresent.RaiseException(ERR_load_BN_strings_procname);
 end;
@@ -164,13 +164,13 @@ begin
   if FuncLoadError then
   begin
     {$if not defined(ERR_load_BN_strings_allownil)}
-    ERR_load_BN_strings := @ERR_ERR_load_BN_strings;
+    ERR_load_BN_strings := ERR_ERR_load_BN_strings;
     {$ifend}
     {$if declared(ERR_load_BN_strings_introduced)}
     if LibVersion < ERR_load_BN_strings_introduced then
     begin
       {$if declared(FC_ERR_load_BN_strings)}
-      ERR_load_BN_strings := @FC_ERR_load_BN_strings;
+      ERR_load_BN_strings := FC_ERR_load_BN_strings;
       {$ifend}
       FuncLoadError := false;
     end;
@@ -179,7 +179,7 @@ begin
     if ERR_load_BN_strings_removed <= LibVersion then
     begin
       {$if declared(_ERR_load_BN_strings)}
-      ERR_load_BN_strings := @_ERR_load_BN_strings;
+      ERR_load_BN_strings := _ERR_load_BN_strings;
       {$ifend}
       FuncLoadError := false;
     end;
